9382b347167d8ad0dfee4ab5c93ac46fe3084617
nnseohnx <nnseohnx@naver.com>
2025-10-05 23:07:33 +0900

    mint function

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -13,7 +13,17 @@ contract MyToken {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
+        // transaction
+        // from, to, data, value, gas, ...
+        _mint(1 * 10 ** uint256(decimals), msg.sender); // 1 MT
     }
+    function _mint(uint256 amount, address owner) internal {
+    // totalSupply = totalSupply + amount;
+    // balanceOf[owner] = balanceOf[owner] + amount;
+    totalSupply += amount;
+    balanceOf[owner] += amount;
+    }
+
 
     //     function totalSupply() external view returns (uint256) {
 //         return totalSupply;
