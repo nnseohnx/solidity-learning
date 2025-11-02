35d6d940615734dfb2eadeb1f91e0f04e43fe460
nnseohnx <nnseohnx@naver.com>
2025-11-02 09:16:40 +0900

    add reward

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -7,33 +7,71 @@
 // - users token management
 // -user --> deposit --> TinyBank --> transfer(user --> TinyBank)
 
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-interface  IMyToken{
+interface IMyToken{
     function transfer(uint256 amount, address to) external;
-
     function transferFrom(address from, address to, uint256 amount) external;
-
+    function mint(uint256 amount, address owner) external;
 }
 
 contract TinyBank {
-    event Staked(address, uint256);
+    event Staked(address from, uint256 amount);
+    event Withdraw(address to, uint256 amount);
 
     IMyToken public stakingToken;
+
+    mapping(address => uint256) public lastClaimedBlock;
+    uint256 rewardPerBlock =1*10**18;
+
     mapping(address => uint256) public staked;
     uint256 public totalStaked;
 
-    constructor(IMyToken _stakingToken) {
-    stakingToken = _stakingToken;
-}
+    address[] public stakedUsers; 
 
+    constructor(IMyToken _stakingToken) {
+        stakingToken = _stakingToken;
+    }
+
+    //who, when?
+    function distributeReward(address to) internal {
+        for (uint i = 0; i < stakedUsers.length; i++) {
+            uint256 blocks = block.number - lastClaimedBlock[to];
+            uint256 reward = (blocks * rewardPerBlock*staked[to])/totalStaked;
+            stakingToken.mint(reward, stakedUsers[i]);
+            lastClaimedBlock[stakedUsers[i]] = block.number;
+        }
+    }
 
     function stake(uint256 _amount) external {
         require(_amount >= 0, "cannot stake 0 amount");
+        distributeReward(msg.sender);
         stakingToken.transferFrom(msg.sender, address(this), _amount);
         staked[msg.sender] += _amount;
         totalStaked += _amount;
+        stakedUsers.push(msg.sender);
         emit Staked(msg.sender, _amount);
     }
-} 
+
+    function withdraw(uint256 _amount) external {
+        require(staked[msg.sender] >= _amount, "insufficient staked token");
+        distributeReward(msg.sender);
+        stakingToken.transfer(_amount, msg.sender);
+        staked[msg.sender] -= _amount;
+        totalStaked -= _amount;
+        if (staked[msg.sender] == 0) {
+            uint256 index;
+            for(uint i = 0; i< stakedUsers.length; i++){
+                if (stakedUsers[i] == msg.sender) {
+                    index = i;
+                    break;
+                }
+            }
+            stakedUsers[index] = stakedUsers[stakedUsers.length-1];
+            stakedUsers.pop();
+
+        }
+        emit Withdraw(msg.sender, _amount);
+    }
+}
