064729485e277ee60a0c0770943567c1af919683
nnseohnx <nnseohnx@naver.com>
2025-11-08 23:42:17 +0900

    add reward test

--- test/TinyBank.ts
+++ test/TinyBank.ts
@@ -1,16 +1,17 @@
 import hre from "hardhat";
-import {expect} from "chai";
-import {DECIMALS, MINTING_AMOUNT} from "./constant";
+import { expect } from "chai";
+import { DECIMALS, MINTING_AMOUNT } from "./constant";
 import { MyToken, TinyBank } from "../typechain-types";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 describe("TinyBank", () => {
-    let signers: HardhatEthersSigner[];
-    let myTokenC: MyToken ;
-    let tinyBankC: TinyBank;
-    beforeEach(async () => {
-     signers = await hre.ethers.getSigners();
-     myTokenC = await hre.ethers.deployContract("MyToken", [
+  let signers: HardhatEthersSigner[];
+  let myTokenC: MyToken;
+  let tinyBankC: TinyBank;
+
+  beforeEach(async () => {
+    signers = await hre.ethers.getSigners();
+    myTokenC = await hre.ethers.deployContract("MyToken", [
       "MyToken",
       "MT",
       DECIMALS,
@@ -19,30 +20,60 @@ describe("TinyBank", () => {
     tinyBankC = await hre.ethers.deployContract("TinyBank", [
       await myTokenC.getAddress(),
     ]);
-});
+  });
 
-    describe("Initialized state check", () => {
-      it("should return totalStaked 0", async () => {
-        expect(await tinyBankC.totalStaked()).equal(0)
-      })
-      it("should return staked 0 amount of signer0", async () => {
-        const signer0 = signers[0];
-        expect(await tinyBankC.stake(signer0.address)).equal(0);
-      });
-      
+  describe("Initialized state check", () => {
+    it("should return totalStaked 0", async () => {
+      expect(await tinyBankC.totalStaked()).to.equal(0n);
+    });
+    it("should return staked 0 amount of signer0", async () => {
+      const signer0 = signers[0];
+      expect(await tinyBankC.staked(signer0.address)).to.equal(0n);
+    });
+  });
+
+  describe("Staking", () => {
+    it("should return staked amount", async () => {
+      const signer0 = signers[0];
+      const stakingAmount = hre.ethers.parseUnits("50", DECIMALS);
+      await myTokenC.approve(await tinyBankC.getAddress(), stakingAmount);
+      await tinyBankC.stake(stakingAmount);
+      expect(await tinyBankC.staked(signer0.address)).to.equal(stakingAmount);
+      expect(await tinyBankC.totalStaked()).to.equal(stakingAmount);
+      expect(
+        await myTokenC.balanceOf(await tinyBankC.getAddress())
+      ).to.equal(await tinyBankC.totalStaked());
     });
 
-    describe("Staking", async() => {
-      it("should return staked amount", async () => {
+    describe("Withdraw", () => {
+      it("should return 0 staked after withdrawing total token", async () => {
         const signer0 = signers[0];
         const stakingAmount = hre.ethers.parseUnits("50", DECIMALS);
         await myTokenC.approve(await tinyBankC.getAddress(), stakingAmount);
         await tinyBankC.stake(stakingAmount);
-        expect(await tinyBankC.staked(signer0.address)).equal(stakingAmount);
-        expect(await tinyBankC. totalStaked()).equal(stakingAmount);
-        expect(await myTokenC.balanceOf(tinyBankC)).equal(
-          await tinyBankC.totalStaked()
-        );
-      }); 
-    })
+        await tinyBankC.withdraw(stakingAmount);
+        expect(await tinyBankC.staked(signer0.address)).to.equal(0n);
+      });
+    });
   });
+
+describe("reward", () => {
+  it("should reward 1MT every blocks", async () => {
+    const signer0 = signers[0];
+    const stakingAmount = hre.ethers.parseUnits("50", DECIMALS);
+    await myTokenC.approve(await tinyBankC.getAddress(), stakingAmount);
+    await tinyBankC.stake(stakingAmount);
+
+    const BLOCKS = 5n;
+    const transferAmount = hre.ethers.parseUnits("1", DECIMALS);
+    for (var i = 0n; i < BLOCKS; i++) {
+      await myTokenC.transfer(transferAmount, signer0.address);
+    }
+
+    await tinyBankC.withdraw(stakingAmount);
+    expect(await myTokenC.balanceOf(signer0.address)).equal(
+      hre.ethers.parseUnits((BLOCKS + MINTING_AMOUNT + 1n).toString())
+    );
+  });
+});
+});
