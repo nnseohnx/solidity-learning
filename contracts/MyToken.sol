7d7792c799644b81ab086819f6746010603483d8
nnseohnx <nnseohnx@naver.com>
2025-11-09 10:44:23 +0900

    add access manager

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,36 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-interface IMyToken{
-    function transfer(uint256 amount, address to) external;
-    function transfrom(address from, address to, uint256 amount) external;
-    function mint(uint256 amount, address owner) external;
-    
-    }
+import "./ManagedAccess.sol";
 
-
-contract MyToken {
+contract MyToken is ManagedAccess {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed spender, uint256 amount);
     
+
     string public name;
     string public symbol;
     uint8 public decimals;
-    uint256 public_amount;
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
-    
 
-    constructor(string memory _name, string memory _symbol,uint8 _decimal, uint256 _amount ) {
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint8 _decimal, 
+        uint256 _amount
+    ) ManagedAccess(msg.sender, msg.sender) {
+      
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
-        // transaction
-        // from, to, data, value, gas, ...
-        _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1 MT
-    }
+       _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1 MT
+}
 
     function approve(address apender, uint256 amount) external {
         allowance[msg.sender][apender] = amount;
@@ -45,28 +42,34 @@ contract MyToken {
         balanceOf[from] -= amount;
         balanceOf[to] += amount;
         emit Transfer(from, to, amount);
-      }
-
-    function mint(uint256 amount, address owner) external{
-        _mint(amount,owner);
-    }  
-
-    function _mint(uint256 amount, address owner) internal {
-        totalSupply += amount;
-        balanceOf[owner] += amount;
-
-        emit Transfer(address(0), owner, amount);
-    }
-
-    function transfer(uint256 amount, address to) external {
-        require(balanceOf[msg.sender] >= amount, 'insufficient balance');
-    balanceOf[msg.sender] -= amount;
-    balanceOf[to] += amount;
-
-    emit Transfer(msg.sender, to, amount);
 }
 
-function assignTransfer(address spender, uint256 amount) external {
+     function mint(uint256 amount, address to) external onlyManager {
+       _mint(amount, to);
+}
+
+     function setManager(address _manager) external onlyOwner{
+       manager = _manager;
+}
+
+     function _mint(uint256 amount, address to) internal {
+       totalSupply += amount;
+       balanceOf[to] += amount;
+
+       emit Transfer(address(0), to, amount);
+}
+
+     function transfer(uint256 amount, address to) external {
+       require(balanceOf[msg.sender] >= amount, "insufficient balance");
+
+       balanceOf[msg.sender] -= amount;
+       balanceOf[to] += amount;
+
+       emit Transfer(msg.sender, to, amount);
+}
+
+
+     function assignTransfer(address spender, uint256 amount) external {
        
         allowance[msg.sender][spender] = amount;
         emit Approval(spender, amount);
